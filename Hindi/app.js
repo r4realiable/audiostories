e00dab4e1f3e4c47a921d628aff62976866146fb
Rahul CV <rahulcv@hotmail.co.in>
2020-12-27 12:24:12 +0530

    Fixed issues

--- Hindi/app.js
+++ Hindi/app.js
@@ -5,14 +5,14 @@ const { v4: uuidv4 } = require('uuid');
 const axios = require('axios');
 
 // uuidv4()
-const audios = require('Audios');
+const audios = require('./Audios');
 const { default: Axios } = require('axios');
 
-
+addToServer(audios);
 async function addToServer(audios){
-    for (i = 0; i < audios.length;i++){
-        const audio = { title: audios[i].title, url: audios[i].name}
-        await axios.post('http://localhost:5001/api/v1/audios',{})
+    for (i = 1; i < audios.length;i++){
+        const audio = { id: uuidv4(),title: audios[i].title, url: `https://r4realiable.github.io/audiostories/Hindi/${audios[i].name}.mp3`, language: 'hindi', artist: 'mama gun', description: audios[i].title}
+        await axios.post('http://localhost:5001/api/v1/audios', audio)
     }
 }
 function renameFiles(){
--- Malayalam/app.js
+++ Malayalam/app.js
@@ -1,8 +1,21 @@
 const fs = require('fs')
 const { join, extname, basename } = require('path');
 const { readdirSync, renameSync } = require('fs');
+const { v4: uuidv4 } = require('uuid');
+const axios = require('axios');
+// uuidv4()
+const audios = require('./Audios');
 
-const files = readdirSync(__dirname)
+addToServer(audios);
+async function addToServer(audios) {
+    for (i = 1; i < audios.length; i++) {
+        const audio = { id: uuidv4(), title: audios[i].title, url: `https://r4realiable.github.io/audiostories/Hindi/${audios[i].name}.mp3`, language: 'malayalam', artist: 'kambi maman', description: audios[i].title }
+        await axios.post('http://localhost:5001/api/v1/audios', audio)
+    }
+}
+function renameFiles() {
+    const files = readdirSync(__dirname)
+
 for (const oldFile of files) {
     const extension = extname(oldFile);
     const name = basename(oldFile, extension);
@@ -16,7 +29,7 @@ for (const oldFile of files) {
     //     renameSync(join(__dirname, oldFile), join(__dirname, 'new' + extension));
     // }
 }
-
+}
 
 function generatefileNames(name){
  const names= name.split(' ');
